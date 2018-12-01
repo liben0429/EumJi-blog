616f55810bd9ac939a9f3a2828e7953d0f472d1f
liben <17718384791@.163.com>
2018-12-01 19:19:37 +0800

    添加启动日志

--- src/main/java/com/eumji/zblog/BlogApplication.java
+++ src/main/java/com/eumji/zblog/BlogApplication.java
@@ -1,5 +1,7 @@
 package com.eumji.zblog;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -18,7 +20,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class BlogApplication {
 
+    private static Logger logger = LoggerFactory.getLogger(BlogApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class,args);
+        logger.info("个人博客服务启动完毕...");
     }
 }
